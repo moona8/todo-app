07418ef7e887a16ff65076dd35b4ee349945d47f
moona8 <73126379+moona8@users.noreply.github.com>
2021-10-02 10:16:26 +0500

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import del from "../src/assets/del.png";
+import edit from "../src/assets/edit.png";
 
-function App() {
+const App = () => {
+  const [todos, updateTodo] = useState([]);
+
+  var inputValue = "";
+  const onInputChange = (event) => {
+    inputValue = event.target.value;
+  };
+
+  const addTodo = () => {
+    updateTodo([...todos, inputValue]);
+    // var input.target.value=""
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    updateTodo(newTodos);
+  };
+  const editTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index] = prompt("");
+
+    updateTodo(newTodos);
+    console.log(index);
+  };
+  // const editTodo=(index, event)=>{
+  //   const newTodos=[...todos];
+  //   newTodos[index]=event .target.value
+  //   if ( event .target.value=== false){
+  //     event .target.value=true
+  //   }
+  // }
+  const delAll = () => {
+    const newTodos = [];
+    updateTodo(newTodos);
+  };
+
+  const items = todos.map((todos, index) => (
+
+    <li className="list-group-item d-flex justify-content-between">
+      {todos} {"  "}
+      <p><button className="btn btn-outline-success mx-20 px-20" onClick={() => { editTodo(index);  }} >{" "}<img src={edit} /> </button>
+      <button className="btn btn-outline-danger  mx-20 px-20" onClick={() => {removeTodo(index);  }} > <img src={del} /></button>
+    </p>
+    </li>
+    
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container  ">
+      <br />
+      <br />
+      <br />
+      <div className=" row justify-content-md-center">
+        <div className="col-md-auto">
+          <h1>
+            <b>Todo App</b>
+          </h1>
+      
+<div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Add Todo" onChange={onInputChange} aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+  <div class="input-group-append">
+    <button class="btn btn-outline-success" type="button" onClick={addTodo}>Add</button>
+  </div>
+</div>
+          <ul className="list-group list-group-flush">{items}</ul>
+        <br />
+          <br />
+          <br />
+          <div className="d-grid gap-2">
+            <button variant="secondary" size="lg" className="btn btn-outline-danger" onClick={delAll}>
+              {" "}
+              Delete All
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
